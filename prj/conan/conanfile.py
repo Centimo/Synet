1f01d32fc3a1705f367cfa11718b7d1da366626d
Centimo <Centimo@users.noreply.github.com>
2026-03-23 02:08:50 +0300

    Fix conan recipe: add simd as transitive dependency in package_info

--- prj/conan/conanfile.py
+++ prj/conan/conanfile.py
@@ -182,6 +182,7 @@ class SynetConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = ["Synet"]
         self.cpp_info.includedirs = ["include"]
+        self.cpp_info.requires = ["simd::simd"]
 
         if int(self.options.perf_level) >= 1:
             self.cpp_info.defines.append("SYNET_PERFORMANCE_STATISTIC")
